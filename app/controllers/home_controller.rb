839d132c7972d3948ca7d267121b4b313b18a4a5
TH <tylerhartland7@gmail.com>
2013-08-11 01:00:23 -0500

    download pdf resume

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def pdf
+    send_file Rails.root.join('app', 'assets', 'Tyler-Hartland.pdf'), type: 'application/pdf'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Tyler::Application.routes.draw do
   root 'home#index'
+  get 'pdf' => 'home#pdf'
   post 'utility_droid/register' => 'utility_droid#register'
   post 'utility_droid/send' => 'utility_droid#send_sms'
 
