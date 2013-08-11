1dd3df0a4cc16f0fabeb4b39cc63e21e0b29fd9d
TH <tylerhartland7@gmail.com>
2013-08-11 01:13:44 -0500

    precompile css assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Tyler::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
-
+  config.assets.precompile += %w( application-screen.css application-print.css )
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
