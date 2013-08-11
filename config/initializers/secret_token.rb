004ccb4c35885af8d25544bca48ec9afac790f5c
TH <tylerhartland7@gmail.com>
2013-08-10 20:49:55 -0500

    rails new . --git -T --database=postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tyler::Application.config.secret_key_base = '86f75f69706e42ba507d498b70a03b7464be111d78c5caaa5c172d485b8fc6ce960f5124b83f406fdac197f41a866718a92d6d97ab78a6369037b3d6b6774a39'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tyler::Application.config.session_store :cookie_store, key: '_tyler_session'
