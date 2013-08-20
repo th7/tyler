d2c661f0ea848a4d01ed12d8b23aa5f90a4a2503
TH <tylerhartland7@gmail.com>
2013-08-19 20:13:21 -0500

    add route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Tyler::Application.routes.draw do
   root 'home#index'
+
+  resources :sms_messages, :only => [:create, :index]
+
   get 'pdf' => 'home#pdf'
   post 'utility_droid/register' => 'utility_droid#register'
   post 'utility_droid/send' => 'utility_droid#send_sms'
