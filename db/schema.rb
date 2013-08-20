ffe38c6735d112986f29a57ce79fed74ff6beea8
TH <tylerhartland7@gmail.com>
2013-08-20 09:40:14 -0500

    updated schema after add sms_messages table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130811021516) do
+ActiveRecord::Schema.define(version: 20130820003614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sms_messages", force: true do |t|
+    t.text     "text"
+    t.string   "from"
+    t.datetime "sent_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string "registration_id"
