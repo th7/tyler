de242acf386302ad012da991298d527af86144dc
TH <tylerhartland7@gmail.com>
2013-08-20 11:56:03 -0500

    current user and authorize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+
+  def current_user
+    @current_user ||= authorize_user
+  end
+
+  def authorize_user
+    user = User.where(username: params[:user][:username]).first
+    if user.password == params[:user][:password]
+      user
+    else
+      nil
+    end
+  end
 end
