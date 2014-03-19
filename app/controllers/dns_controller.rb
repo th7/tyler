c6eb10fd9a0d43f050b82e783e32fd4d91f21f11
Tyler Hartland <tyler.hartland@code42.com>
2014-03-18 23:12:24 -0500

    add example curl get to dns_controller

--- app/controllers/dns_controller.rb
+++ app/controllers/dns_controller.rb
@@ -8,7 +8,14 @@ class DnsController < ApplicationController
       render json: 'none found', status: 404
     end
   end
-
+=begin
+  curl -vv -u username:password -X GET -H "Accept: application/json" -H "Content-Type: application/json" -d '{
+  "user": {
+    "username": "username",
+    "password": "password"
+  }
+}' http://localhost:3000/dns/a_new_url
+=end
   def create
     if entry = DnsEntry.where(url: dns_entry_params[:url]).first
       entry.update_attributes(dns_entry_params)
